96b6c7c633ca1e702f30ac2ad9edfe65816d2999
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-01-21 17:44:12 -0500

    Implement book loading from JSON and display functionality in console application

--- LinqQueries.cs
+++ LinqQueries.cs
@@ -1,5 +1,25 @@
 namespace linq;
 
 public class LinqQueries {
-  public LinqQueries() { }
+  private List<Book> librosCollection = new List<Book>();
+  public LinqQueries() {
+    StreamReader reader = new StreamReader("books.json");
+
+    string json = reader.ReadToEnd();
+
+    var deserializedBooks = System.Text.Json.JsonSerializer
+      .Deserialize<List<Book>>(
+        json, new System.Text.Json.JsonSerializerOptions() 
+        {
+          PropertyNameCaseInsensitive = true
+        }
+      );
+
+    librosCollection = deserializedBooks ?? new List<Book>();
+  }
+
+  // Retornar IEnumerable siempre que los datos no se vayan a modificar o se requiera un manejo de datos más específico
+  public IEnumerable<Book> AllBooks() {
+    return librosCollection;
+  }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using linq;
+
+LinqQueries queries = new();
+
+ShowBooks(queries.AllBooks());
+
+void ShowBooks(IEnumerable<Book> books) {
+  // {0, -70} -> 0: Title, -70: 70 espacios a la izquierda
+  // {1, 7} -> 1: Pages, 7: 7 espacios a la derecha
+  // {2, 15} -> 2: Status, 15: 15 espacios a la derecha
+  // \n -> Salto de línea
+  // entre cada columna se puede poner un caracter para separarlas -> Console.WriteLine("{0, -70} | {1, 7} | {2, 15}\n", "Title", "Pages", "Status");
+  Console.WriteLine("{0, -70} {1, 7} {2, 15}\n", "Title", "Pages", "Published Date");
+
+  foreach (var book in books)
+  {
+    // para fechas, si solo se quiere mostrar la fecha se puede usar book.PublishedDate.ToShortDateString()
+    Console.WriteLine("{0, -70} {1, 7} {2, 15}", book.Title, book.PageCount, book.PublishedDate.ToShortDateString());
+  }
+}
